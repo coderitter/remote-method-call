f3834d7446d36fe0b2c74293a226413b173fbe23
R Gastmeier <r.gastmeier@gmail.com>
2020-09-13 09:55:54 +0200

    Move dependencies for testing into dev

--- test/Result.test.ts
+++ test/Result.test.ts
@@ -6,8 +6,11 @@ import Result from '../src/Result'
 describe('Result', function() {
   describe('misfits', function() {
     it('should create an instance of the calling class', function() {
-      let testValue: TestResult = TestResult.misfits(new Misfit)
-      expect(testValue).to.be.instanceOf(TestResult)
+      let testResult1: TestResult = TestResult.misfits(new Misfit)
+      expect(testResult1).to.be.instanceOf(TestResult)
+
+      let testResult2: TestResult = TestResult.misfits([ new Misfit ])
+      expect(testResult2).to.be.instanceOf(TestResult)
     })
   })
 })
